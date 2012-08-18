98ae031dbc266d2bf70ac4bf13f1a40edcf045dd
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-08-19 05:20:31 +0900

    Add foreign key

--- db/migrate/20120818194220_create_books.rb
+++ db/migrate/20120818194220_create_books.rb
@@ -4,10 +4,12 @@ class CreateBooks < ActiveRecord::Migration
       t.string :title
       t.string :author
       t.string :file
+      t.integer :user_id
 
       t.timestamps
     end
 
+    add_index :books, :user_id
     add_index :books, :title
     add_index :books, :author
     add_index :books, :file, unique: true
