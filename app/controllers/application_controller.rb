ec62494ace8511283f94aa5b5307273f5cc5e601
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-08-19 06:14:54 +0900

    Add codes around password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_user
+    if sessioin[:user_id]
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
+  end
+  helper_method :current_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :username
+  attr_accessible :username
+  attr_accessor :password
 
   has_many :books, dependent: :destroy
+
+  def authenticate(uncrypted_password)
+    BCrypt::Password.new(password_digest) == uncrypted_password
+  end
+
+  before_save do
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
