15d19ce94222ef10565ac38e7ce73a05bc3eef36
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-08-19 05:31:28 +0900

    Add indices for contents

--- db/migrate/20120818201818_create_contents.rb
+++ db/migrate/20120818201818_create_contents.rb
@@ -1,10 +1,17 @@
 class CreateContents < ActiveRecord::Migration
-  def change
-    create_table :contents do |t|
+  def up
+    create_table :contents, options: 'ENGINE=mroonga' do |t|
       t.text :content
       t.integer :book_id
 
       t.timestamps
     end
+
+    add_index :contents, :book_id
+    execute 'create fulltext index `fulltext_index_on_content` on contents(content)'
+  end
+
+  def down
+    drop_table :contents
   end
 end
