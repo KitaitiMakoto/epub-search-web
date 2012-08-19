da439006bd28bbf8b46dc4a6ef7a7f2bb667a5b7
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-08-19 21:54:48 +0900

    Move contents under book

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 EpubSearch::Application.routes.draw do
-  resources :contents, only: [:index, :show]
-  resources :books, only: [:index, :show, :new, :create, :destroy]
+  resources :books, only: [:index, :show, :new, :create, :destroy] do
+    resources :contents, only: [:index, :show]
+  end
   resources :users, only: [:new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
 end
