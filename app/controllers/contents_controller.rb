cb2e9d9bcbf8d141e704b7f4ce3ceb886b144a45
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-08-20 04:41:37 +0900

    Add search function

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,7 +2,14 @@ class ContentsController < ApplicationController
   # GET /contents
   # GET /contents.json
   def index
-    @contents = Content.all
+    @q = params[:q]
+    if @q.blank?
+      @contents = []
+    else
+      @contents = Content.joins(:book).where(books: {user_id: current_user.id})\
+        .where(['MATCH (content) AGAINST (?)', @q])\
+        .order(:contents => ['MATCH (content) AGAINST (?) DESC', @q])
+    end
 
     respond_to do |format|
       format.html # index.html.erb
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 EpubSearch::Application.routes.draw do
-  resources :books, only: [:index, :show, :new, :create, :destroy] do
-    resources :contents, only: [:index, :show]
-  end
+  resources :contents, only: [:index]
+  resources :books, only: [:index, :show, :new, :create, :destroy]
   resources :users, only: [:new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
 end
