50a3e8cbaa062f68c7c6af7be250352e45663921
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-08-19 19:48:05 +0900

    Add filtering by user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.where(:user_id => current_user.id)
 
     respond_to do |format|
       format.html # index.html.erb
