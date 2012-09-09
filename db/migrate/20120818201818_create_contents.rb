7e550afd444c2b91b225597441e582497247bb1b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-09-10 06:29:05 +0900

    Use extended `add_index` instead of `execute`

--- db/migrate/20120818201818_create_contents.rb
+++ db/migrate/20120818201818_create_contents.rb
@@ -9,7 +9,7 @@ class CreateContents < ActiveRecord::Migration
 
     add_index :contents, :book_id, null: false
     add_foreign_key :contents, :books
-    execute 'create fulltext index `fulltext_index_on_content` on contents(`content`) COMMENT \'parser "TokenMecab"\''
+    add_index :contents, :content, fulltext: true, comment: 'parser "TokenMecab"'
   end
 
   def down
